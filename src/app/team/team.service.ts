4af72a785cc057b94138a67d9f835567986aaded
PabloA.dv2 <pablo.amil@hmcts.net>
2017-11-21 14:26:34 +0000

    rxjs fix test

--- src/app/team/team.service.ts
+++ src/app/team/team.service.ts
@@ -6,6 +6,9 @@ import { BaseService } from '../shared/services/base.service';
 import { ShareDataService } from '../shared/services/shared-data.service';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class TeamService extends BaseService {
