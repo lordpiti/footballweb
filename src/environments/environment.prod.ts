674a5ddf1f0b4a1c4fa0056b532554da27d99140
Pablo Amil <eltiodelassortijas@gmail.com>
2018-08-27 05:39:17 +0100

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  api_url: 'https://footballpitiapi.azurewebsites.net/api/',
+  api_url: 'https://footballsandbox.azurewebsites.net/api/',
   FACEBOOK_APP_ID: '2050633918500176',
   GOOGLE_CLIENT_ID: '357813264391-bc51b2u0ohaeb6v78k2b2tpr5pdi6c09.apps.googleusercontent.com',
   home_url: 'https://footballtpititest.herokuapp.com',
-  hubUrl: 'https://footballpitiapi.azurewebsites.net'
+  hubUrl: 'https://footballsandbox.azurewebsites.net'
 };
