0844e2d8a8fe5afc2b76edfca1c1ee691f500869
Pablo Amil <lordpiti@gmail.com>
2020-06-24 01:59:57 +0100

    updated api urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule} from './app-routes.module';
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
-import { ModalModule } from 'ngx-modialog';
-import { BootstrapModalModule } from 'ngx-modialog/plugins/bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ShareDataService } from './shared/services/shared-data.service';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { FacebookModule } from 'ngx-facebook';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { TeamModule } from './team/team.module';
-import { SampleService } from 'angular-piti-module-test';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AuthInterceptor } from './authentication/authInterceptor';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreModule } from '@ngrx/store';
-import { AuthGuard } from './authentication/auth-guard';
-import { EffectsModule } from '@ngrx/effects';
-import { team } from './core/reducers/team';
-import { environment } from '../environments/environment';
-import { TeamActions } from './core/actions/teamAction';
-import { TeamEffects } from './core/effects/teamEffects';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { ApolloBoostModule, ApolloBoost } from 'apollo-angular-boost';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { SharedModule } from './shared/shared.module';
-import { DropzoneModule, DROPZONE_CONFIG, DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
-
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppRoutingModule } from "./app-routes.module";
+import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { HomeComponent } from "./home/home.component";
+import { ModalModule } from "ngx-modialog";
+import { BootstrapModalModule } from "ngx-modialog/plugins/bootstrap";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ShareDataService } from "./shared/services/shared-data.service";
+import { BsDropdownModule } from "ngx-bootstrap";
+import { FacebookModule } from "ngx-facebook";
+import { AuthenticationModule } from "./authentication/authentication.module";
+import { TeamModule } from "./team/team.module";
+import { SampleService } from "angular-piti-module-test";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { AuthInterceptor } from "./authentication/authInterceptor";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreModule } from "@ngrx/store";
+import { AuthGuard } from "./authentication/auth-guard";
+import { EffectsModule } from "@ngrx/effects";
+import { team } from "./core/reducers/team";
+import { environment } from "../environments/environment";
+import { TeamActions } from "./core/actions/teamAction";
+import { TeamEffects } from "./core/effects/teamEffects";
+import { MatButtonModule, MatToolbarModule } from "@angular/material";
+import { OverlayModule } from "@angular/cdk/overlay";
+import { ApolloBoostModule, ApolloBoost } from "apollo-angular-boost";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { SharedModule } from "./shared/shared.module";
+import {
+  DropzoneModule,
+  DROPZONE_CONFIG,
+  DropzoneConfigInterface,
+} from "ngx-dropzone-wrapper";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PageNotFoundComponent
-  ],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -63,27 +61,30 @@ import { DropzoneModule, DROPZONE_CONFIG, DropzoneConfigInterface } from 'ngx-dr
     StoreModule.forRoot({ team }),
     EffectsModule.forRoot([TeamEffects]),
     StoreDevtoolsModule.instrument({
-      maxAge: 25 //  Retains last 25 states
+      maxAge: 25, //  Retains last 25 states
     }),
     ApolloBoostModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
   ],
   providers: [
     ShareDataService,
-    SampleService, {
+    SampleService,
+    {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
+      multi: true,
     },
     AuthGuard,
-    TeamActions
+    TeamActions,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor(boost: ApolloBoost) {
     boost.create({
-      uri: 'https://footballsandbox.azurewebsites.net/graphql'
+      uri: "https://thetestfor31.azurewebsites.net/graphql",
     });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,15 @@
 export const environment = {
   production: false,
   //api_url: 'http://localhost:57544/api/',
-  api_url: 'https://footballsandbox.azurewebsites.net/api/',
-  FACEBOOK_APP_ID: '2050633918500176',
-  GOOGLE_CLIENT_ID: '357813264391-bc51b2u0ohaeb6v78k2b2tpr5pdi6c09.apps.googleusercontent.com',
-  home_url: 'http://localhost:4200',
+  api_url: "https://thetestfor31.azurewebsites.net/api/",
+  FACEBOOK_APP_ID: "2050633918500176",
+  GOOGLE_CLIENT_ID:
+    "357813264391-bc51b2u0ohaeb6v78k2b2tpr5pdi6c09.apps.googleusercontent.com",
+  home_url: "http://localhost:4200",
   //hubUrl: 'http://localhost:57544',
-  hubUrl: 'https://footballsandbox.azurewebsites.net',
-  home_rss_feed_url: 'http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml',
-  rss2json_base_url: 'https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url='
+  hubUrl: "https://thetestfor31.azurewebsites.net",
+  home_rss_feed_url:
+    "http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml",
+  rss2json_base_url:
+    "https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url=",
 };
