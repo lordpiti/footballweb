5ba1b6df849cda617c917c7de0fd3d08d9615761
Pablo Amil <lordpiti@gmail.com>
2021-01-16 21:47:44 +0000

    removed friends permission for facebook login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, NgZone, AfterViewInit } from '@angular/core';
-import { GoogleAuthService } from '../google-auth.service';
-import { AppGlobals } from '../app-globals';
-import { environment } from '../../../environments/environment';
-import { UserService } from '../user.service';
+import { Component, OnInit, NgZone, AfterViewInit } from "@angular/core";
+import { GoogleAuthService } from "../google-auth.service";
+import { AppGlobals } from "../app-globals";
+import { environment } from "../../../environments/environment";
+import { UserService } from "../user.service";
 import {
   FacebookService,
   InitParams,
   LoginOptions,
   LoginResponse,
-} from 'ngx-facebook';
+} from "ngx-facebook";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements AfterViewInit, OnInit {
   imageURL: string;
@@ -32,7 +32,7 @@ export class LoginComponent implements AfterViewInit, OnInit {
     const initParams: InitParams = {
       appId: environment.FACEBOOK_APP_ID,
       xfbml: true,
-      version: 'v2.8',
+      version: "v2.8",
     };
 
     fb.init(initParams);
@@ -55,7 +55,7 @@ export class LoginComponent implements AfterViewInit, OnInit {
     const loginOptions: LoginOptions = {
       enable_profile_selector: true,
       return_scopes: true,
-      scope: 'public_profile,user_friends,email',
+      scope: "public_profile,email",
     };
 
     this.fb
@@ -70,15 +70,15 @@ export class LoginComponent implements AfterViewInit, OnInit {
           .subscribe((data) => {
             if (data) {
               // Setting data to localstorage.
-              localStorage.setItem('token', response.authResponse.accessToken);
-              localStorage.setItem('image', '');
-              localStorage.setItem('name', data.name);
-              localStorage.setItem('email', data.email);
-              localStorage.setItem('authenticationType', '1');
-              localStorage.setItem('role', data.role);
+              localStorage.setItem("token", response.authResponse.accessToken);
+              localStorage.setItem("image", "");
+              localStorage.setItem("name", data.name);
+              localStorage.setItem("email", data.email);
+              localStorage.setItem("authenticationType", "1");
+              localStorage.setItem("role", data.role);
 
               this.token = response.authResponse.accessToken;
-              this.imageURL = '';
+              this.imageURL = "";
               this.name = data.name;
               this.email = data.email;
               this.role = data.role;
@@ -94,9 +94,9 @@ export class LoginComponent implements AfterViewInit, OnInit {
 
   getProfile() {
     this.fb
-      .api('/me')
+      .api("/me")
       .then((res: any) => {
-        console.log('Got the users profile', res);
+        console.log("Got the users profile", res);
       })
       .catch(this.handleError);
   }
@@ -104,7 +104,7 @@ export class LoginComponent implements AfterViewInit, OnInit {
   loginWithGoogle(): void {
     setTimeout(() => {
       this._googleAuth.authenticateUser((result) => {
-        localStorage.setItem('authenticationType', '2');
+        localStorage.setItem("authenticationType", "2");
         this.token = result;
         // Using Angular2 Zone dependency to manage the scope of variables
         this.zone.run(() => {
@@ -115,29 +115,29 @@ export class LoginComponent implements AfterViewInit, OnInit {
   }
 
   private handleError(error) {
-    console.error('Error processing action', error);
+    console.error("Error processing action", error);
   }
 
   getData() {
     setTimeout(() => {
-      this.imageURL = localStorage.getItem('image');
-      this.name = localStorage.getItem('name');
-      this.email = localStorage.getItem('email');
-      this.authenticationType = localStorage.getItem('authenticationType');
+      this.imageURL = localStorage.getItem("image");
+      this.name = localStorage.getItem("name");
+      this.email = localStorage.getItem("email");
+      this.authenticationType = localStorage.getItem("authenticationType");
 
       if (this.token) {
         this.userService.loginUserGoogle(this.token).subscribe((data) => {
           if (data) {
             this.role = data.role;
-            localStorage.setItem('role', data.role);
-            localStorage.setItem('token', data.token);
+            localStorage.setItem("role", data.role);
+            localStorage.setItem("token", data.token);
           } else {
             this.token = null;
             this.clearLocalStorage();
           }
         });
       } else {
-        const existingToken = localStorage.getItem('token');
+        const existingToken = localStorage.getItem("token");
         this.token = existingToken;
       }
     }, 50);
@@ -147,7 +147,7 @@ export class LoginComponent implements AfterViewInit, OnInit {
    * Logout user and calls function to clear the localstorage
    */
   logout() {
-    if (this.authenticationType === '2') {
+    if (this.authenticationType === "2") {
       const scopeReference = this;
       this._googleAuth.userLogout(function () {
         scopeReference.clearLocalStorage();
@@ -163,11 +163,11 @@ export class LoginComponent implements AfterViewInit, OnInit {
    * Clearing Localstorage of browser
    */
   clearLocalStorage() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('image');
-    localStorage.removeItem('name');
-    localStorage.removeItem('email');
-    localStorage.removeItem('authenticationType');
-    localStorage.removeItem('role');
+    localStorage.removeItem("token");
+    localStorage.removeItem("image");
+    localStorage.removeItem("name");
+    localStorage.removeItem("email");
+    localStorage.removeItem("authenticationType");
+    localStorage.removeItem("role");
   }
 }
